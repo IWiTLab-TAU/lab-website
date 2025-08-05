06362cde264d66e91928cf37725c211efc4b33a3
yoavbenyaakov <yoavb1@mail.tu.ac.il>
2025-08-05 20:18:29 +0300

    intial commit

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('projects/', views.projects, name='projects'),
+    path('publications/', views.publications, name='publications'),
+    path('teaching/', views.teaching, name='teaching'),
+    path('awards/', views.awards, name='awards'),
+    path('contact/', views.contact, name='contact'),
+]
