b199bb588c0fb15a62c51f61eefe901d5c000af0
yoavbenyaakov <yoavb1@mail.tu.ac.il>
2025-08-05 20:39:37 +0300

    contact

--- LabWebsite/settings.py
+++ LabWebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8n6a8=4mnv)ax0h+a25z)ovc!+(th(2oyjd@6hn4jsixjb+$hd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [''iwitlabtau.pythonanywhere.com'']
 
 
 # Application definition
